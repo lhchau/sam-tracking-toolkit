0caa0ae12ac9661d7083222db6cbd8e1e43d308c
lhchau <lhchau20@apcs.fitus.edu.vn>
2023-10-20 11:32:04 +0700

    feat: ResNet + LeakyRelu

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, activation='relu'):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -30,19 +30,23 @@ class BasicBlock(nn.Module):
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
+        if activation == 'relu':
+            self.activate = F.relu()
+        elif activation == 'leaky_relu':
+            self.activate = F.leaky_relu()
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.activate(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.activate(out)
         return out
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, activation='relu'):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -60,40 +64,50 @@ class Bottleneck(nn.Module):
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
             )
+        if activation == 'relu':
+            self.activate = F.relu()
+        elif activation == 'leaky_relu':
+            self.activate = F.leaky_relu()
+            
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.activate(self.bn1(self.conv1(x)))
+        out = self.activate(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.activate(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, activation='relu'):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, activation=activation)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, activation=activation)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, activation=activation)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, activation=activation)
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        
+        if activation == 'relu':
+            self.activate = F.relu()
+        elif activation == 'leaky_relu':
+            self.activate = F.leaky_relu()
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, activation):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, activation))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x, return_features=False, return_block=5):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.activate(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         if return_features and return_block == 1:
             return out
